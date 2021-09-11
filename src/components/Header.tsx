95b7887af6304bc334901d3902ae9581a326384b
William Grohe <william.grohe@gmail.com>
2021-09-11 02:30:33 -0300

    Finalizado animações header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,15 @@
 import '../styles/header.scss'
 
-/* abrir e esconder o menu toggle/hamburguer */
-// const nav = document.querySelector('#header nav')
-// const toggle = document.querySelectorAll('nav .toggle')
-
-// function heandleMenuToggle() {
-//   for (let element of toggle) {
-
-//     element.addEventListener('onClick', () => {
-//       nav.classList.toggle('show')
-//     })
-//   }
-// }
-
-// for (let element of toggle) {
-//   element.addEventListener('click', () => {
-//     nav.classList.toggle('show')
-//   })
-// }
-
 export function Header() {
 
   const nav = document.querySelector('#header nav')
 
-  function heandleMenuOpen() {
-    if(!nav?.classList.contains('show')){
-      nav?.classList.add('show');
-    } 
+  function heandleMenuToggle () {
+    nav?.classList.toggle('show');
   }
 
-  function heandleMenuClose() {
-    if(nav?.classList.contains('show')){
-      nav?.classList.remove('show');
-    } 
+  function heandleMenuClose(){
+    nav?.classList.remove('show');
   }
 
   return (
@@ -41,15 +18,15 @@ export function Header() {
         <a className="logo" href="#home">pet<span>stop</span>.</a>
         <div className="menu">
           <ul className="grid">
-            <li><a className="title" href="#home">Início</a></li>
-            <li><a className="title" href="#about">Sobre</a></li>
-            <li><a className="title" href="#services">Serviços</a></li>
-            <li><a className="title" href="#testemonials">Depoimentos</a></li>
-            <li><a className="title" href="#contact">Contato</a></li>
+            <li><a className="title" onClick={heandleMenuClose} href="#home">Início</a></li>
+            <li><a className="title" onClick={heandleMenuClose} href="#about">Nova Entrega</a></li>
+            <li><a className="title" onClick={heandleMenuClose} href="#services">Agenda</a></li>
+            <li><a className="title" onClick={heandleMenuClose} href="#testemonials">...</a></li>
+            <li><a className="title" onClick={heandleMenuClose} href="#contact">...</a></li>
           </ul>
         </div>
-        <div className="toggle icon-menu" onClick={heandleMenuOpen}><i className="fas fa-bars"></i></div>
-        <div className="toggle icon-cross" onClick={heandleMenuClose}><i className="fas fa-times"></i></div>
+        <div className="toggle icon-menu" onClick={heandleMenuToggle}><i className="fas fa-bars"></i></div>
+        <div className="toggle icon-cross" onClick={heandleMenuToggle}><i className="fas fa-times"></i></div>
       </nav>
     </header>
   );
